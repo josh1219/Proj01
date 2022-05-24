85c9c73bab03f4c6fb514d73c2ae21b543287e9c
seunghyeon Cho <elk1219@naver.com>
2022-05-24 19:36:07 +0900

    2022.05.24

--- src/main/webapp/member/Scripts-Oracle.sql
+++ src/main/webapp/member/Scripts-Oracle.sql
@@ -1,22 +1,17 @@
 Create table t_member (
-    id varchar2(10) Primary Key,
-    pwd varchar2(10) not null,
+    id varchar2(10) primary key,
+    pwd varchar2(10) not null, 
     name varchar2(50) not null,
-    email varchar2(100) null,
+    email varchar2(100) null, 
     joinDate date default sysdate
     );
     
--- 더미 데이터 입력
+-- 더미 데이터 입력 
 insert into t_member 
-values ('hong','1234','홍길동','hong@gmail.com',sysdate);
-
-
+values('hong', '1234', '홍길동', 'hong@gmail.com', sysdate); 
 insert into t_member 
-values ('lee','1234','이순신','lee@gmail.com',sysdate);
-
-
+values('lee', '1234', '이순신', 'lee@gmail.com', sysdate); 
 insert into t_member 
-values ('kim','1234','김유신','kim@gmail.com',sysdate);
-
-commit;
-select * from t_member;
+values('kim', '1234', '김유신', 'kim@gmail.com', sysdate); 
+commit; 
+select * from t_member; 
